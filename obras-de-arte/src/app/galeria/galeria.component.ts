f1311b8dc9ebea45441476539547db4c152c6c8f
AnaTRJesus <anaribeiro183@gmail.com>
2018-04-18 20:58:53 +0100

    Criação da estrutura

---
+++ obras-de-arte/src/app/galeria/galeria.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria',
+  templateUrl: './galeria.component.html',
+  styleUrls: ['./galeria.component.css']
+})
+export class GaleriaComponent implements OnInit {
+
+
+  imagens: string[] = [
+    "Imagem01",
+    "Imagem02",
+    "Imagem03",
+    "Imagem04",
+    "Imagem05",
+    "Imagem06",
+    "Imagem07",
+    "Imagem08"
+  ];
+
+    mostrarimagem: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public esconderImagem(){
+
+    this.mostrarimagem = false;
+  }
+
+  public mostrarImagem(){
+
+    this.mostrarimagem = true;
+  }
+
+}
